3cba0f2fe56f50099f9126019a0bbad13dfb441f
PPJQ <128625072+PPJQ@users.noreply.github.com>
2024-02-08 23:11:01 -0600

    Complementación y Descripción del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PF-Sprint5
-Proyecto Final del Sprint 5 del curso Analista de Datos impartido por TripleTen
+Proyecto Final del Sprint 5 del curso Analista de Datos impartido por TripleTen.
+
+En este proyecto podemos ver gráficas que representan los siguientes parámetros:
+
+* Un histograma que nos muestra la cantidad de vehículos por marca.
+* Un gráfico de dispersión que relaciona el precio y la marca del vehículo.
+
+Adicional a esto, podemos crear un gráfico de dispersión donde podemos ver la relación de kilometraje y precio de la lista de vehículos.
+
--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-# Importación de librerías:
+# ======= Importación de librerías =======
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Encabezado de la aplicación:
+# ======= Encabezado de la aplicación =======
 st.header('Ventas de vehículos')
 
-# Leemos los datos
+# ======= Lectura de datos =======
 car_data = pd.read_csv('vehicles_us.csv')
 
-# Creamos un botón para crear un histograma
-hist_button = st.button('Construir histograma')
 
-# Al hacer click en el botón se hará lo siguiente
-if hist_button:
+# ======= Gráfica 1: Vehículos por Marca =======
+
+# Creamos una casilla de verificación para crear un histograma
+histogram_check = st.checkbox('Construir histograma')
+
+# Si la casilla de "histogram_check" esta marcada, se hará un histograma
+if histogram_check:
     # Escribir un mensaje:
     st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        'Conteo de Vehículos por Marca')
 
     # Creamos un histograma:
     fig = px.histogram(car_data, x='model')
 
     # Creamos un gráfico interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+
+# ======= Gráfica 2: Costo por Marca =======
 
 # Creamos una casilla de verificación para crear un gráfico de dispersión
 dispersion_check = st.checkbox('Construir gráfico de dispersión')
@@ -31,10 +37,28 @@ dispersion_check = st.checkbox('Construir gráfico de dispersión')
 if dispersion_check:
     # Escribir un mensaje:
     st.write(
-        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+        'Relación Costo por Marca')
 
     # Creamos el gráfico de dispersión:
     fig_disp = px.scatter(car_data, x='model', y='price')
 
     # Creamos un gráfico interactivo
     st.plotly_chart(fig_disp, use_container_width=True)
+
+
+# ======= Gráfica 3: Kilometraje/Precio =======
+
+# Creamos un botón para crear un gráfico de dispersión Kilometraje/Precio
+hits_button = st.button('Kilometraje/Precio')
+
+# Si se presiona el botón se creara un gráfico de dispersión relacionando Kilometraje/Precio
+if hits_button:
+    # Escribir un mensaje:
+    st.write(
+        'Kilometraje/Precio')
+
+    # Creamos un gráfico de dispersión
+    fig_km_price = px.scatter(car_data, x='odometer', y='price')
+
+    # Creamos un gráfico interactivo:
+    st.plotly_chart(fig_km_price, use_container_width=True)
